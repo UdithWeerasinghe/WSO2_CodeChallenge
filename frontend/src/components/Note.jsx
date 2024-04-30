9fd905baf0ddcb28de7808e201771be4fc1a1a25
Udith Weerasinghe <118172498+UdithWeerasinghe@users.noreply.github.com>
2024-04-30 18:08:41 +0530

    WSO2 CODE CHALLENGE

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,31 @@
+//import React from "react";
+import PropTypes from "prop-types"; // Import PropTypes
+import "../styles/Note.css";
+
+function Note({ note, onDelete }) {
+    const formattedDate = new Date(note.created_at).toLocaleDateString("en-US");
+
+    return (
+        <div className="note-container">
+            <p className="note-title">{note.title}</p>
+            <p className="note-content">{note.content}</p>
+            <p className="note-date">{formattedDate}</p>
+            <button className="delete-button" onClick={() => onDelete(note.id)}>
+                Delete
+            </button>
+        </div>
+    );
+}
+
+// Add prop type validation for the 'note' and 'onDelete' props
+Note.propTypes = {
+    note: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        content: PropTypes.string.isRequired,
+        created_at: PropTypes.string.isRequired // Adjust the type according to the actual type of 'created_at'
+    }).isRequired,
+    onDelete: PropTypes.func.isRequired
+};
+
+export default Note;
